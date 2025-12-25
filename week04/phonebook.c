522427715d308e928e6ab518382f716d8d0142e9
Reyan A <iamreyan@proton.me>
2025-12-25 11:39:56 +0500

    week 05 started

--- week04/phonebook.c
+++ week04/phonebook.c
@@ -5,14 +5,14 @@
 int main(void)
 {
     // Open CSV file
-    FILE *file = fopen(phonebook.csv, a);
+    FILE *file = fopen(phonebook.csv, "a");
 
     // Get name and number
-    char *name = get_string(Name: );
-    char *number = get_string(Number: );
+    char *name = get_string("Name: ");
+    char *number = get_string("Number: ");
 
     // Print to file
-    fprintf(file, %s,%sn, name, number);
+    fprintf(file, "%s,%sn", name, number);
 
     // Close file
     fclose(file);
